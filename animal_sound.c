eaf123c807953d4f35f4ceee670cd7426d64f81c
Mohamed Saied <infint_hero@hotmail.com>
2025-09-18 18:13:13 +0300

    initial commit

---
+++ animal_sound.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void cow_sound()
+{
+ printf("COW Sound\n");
+}
+
+void cat_sound()
+{
+ printf("CAT Sound\n");
+}
+void dog_sound()
+{
+ printf("DOG Sound\n");
+}
+enum Sound
+{
+    COW,
+    CAT,
+    DOG
+};
+int main()
+{
+    //condition to switch on sound
+    int choice;
+    printf("Enter your choice:\n");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case COW:
+        cow_sound();
+        break;
+    case CAT:
+        cat_sound();
+        break;
+    case DOG:
+        dog_sound();
+        break;
+    default:
+        break;
+    }
+}
